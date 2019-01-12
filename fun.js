7309becbca35cb34e7170c7b833df81bb15557dd
Shree Hari <shreeh1@umbc.edu>
2019-01-11 19:28:35 -0500

    major changes

--- fun.js
+++ fun.js
@@ -20,3 +20,51 @@ typewriter.typeString('Coder')
     .pauseFor(2000)
     .start();
 
+
+
+// to hover the side bar on click
+var element = document.querySelector("#sidenav");
+var element1 = document.querySelector("#fa-bars");
+var home = document.querySelector("#home");
+var dark = document.querySelector("#dark");
+var projects = document.querySelector("#projects");
+var projectdark = document.querySelector("#projectdark");
+var passion = document.querySelector("#passion");
+var passiondark = document.querySelector("#passiondark");
+var reach_out = document.querySelector("#reach_out");
+
+// the bar click to bring the side bar to visibility
+element1.addEventListener('click',function(){
+    element.style.left = 0;
+});
+
+home.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+dark.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+projects.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+projectdark.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+passion.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+passiondark.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+reach_out.addEventListener('click', function(){
+    element.style.left = "-384px";
+});
+
+
+
